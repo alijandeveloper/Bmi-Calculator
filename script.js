8150493cce287ccb9bfd8367dd2588c616f004f8
Ali Jan <alijandeveloper15@gmail.com>
2025-03-05 12:05:44 -0800

    getElement

---
+++ script.js
@@ -0,0 +1,7 @@
+const weightInput = document.getElementById("weight");
+const heightInput = document.getElementById("height");
+const calculateButton = document.getElementById("calculate");
+const resetButton = document.getElementById("reset");
+const resultDiv = document.getElementById("result");
+const historyList = document.getElementById("history");
+const themeButton = document.getElementById("theme-button");
